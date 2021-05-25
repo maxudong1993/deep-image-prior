45e8d91efd912ef71fc298ea9cf29ea1017afdff
maxudong1993 <xudongma@MacBook-Pro-8.local>
2021-05-25 13:46:53 +0800

    Update common_utils.py

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -290,7 +290,7 @@ def np_to_pil(img_np):
     
     From C x W x H [0..1] to  W x H x C [0...255]
     '''
-    ar = np.clip(img_np*255，0，255).astype(np.uint16)
+    ar = np.clip(img_np*255,0,255).astype(np.uint16)
 #     ar = img_np.astype(np.uint16)
     
     if img_np.shape[0] == 1:
